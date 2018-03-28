8c3581bf352c1732df6c1fbe380c6b0677aabc82
Paul Balaji <paulmonish@gmail.com>
2018-03-28 19:46:31 +0100

    drone now has a fixed target that it will move towards
    Signed-off-by: Paul Balaji <paulmonish@gmail.com>

--- codebase/workers/unity/Assets/Gamelogic/Drone/DroneBehaviour.cs
+++ codebase/workers/unity/Assets/Gamelogic/Drone/DroneBehaviour.cs
@@ -1,17 +1,49 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using Improbable;
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
 
-public class DroneBehaviour : MonoBehaviour
+namespace Assets.Gamelogic.Drone
 {
-    // Use this for initialization
-    void Start()
+    [WorkerType(WorkerPlatform.UnityWorker)]
+    public class DroneBehaviour : MonoBehaviour
     {
+        [Require]
+        private Position.Writer PositionWriter;
 
-    }
+        private Vector3 target = new Vector3(0, 0, 0);
+        private float speed = 0.5f;
 
-    // Update is called once per frame
-    void Update()
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+
+		void FixedUpdate()
+		{
+            Vector3 direction = target - transform.position;
+            direction.Normalize();
+
+            transform.position += direction * speed * Time.deltaTime;
+            //Debug.Log("fixed update: " + direction + "");
+
+            updatePosition();
+		}
+
+        private void updatePosition()
+        {
+            PositionWriter.Send(new Position.Update().SetCoords(transform.position.ToCoordinates()));
+        }
+	}
+
+    public static class Vector3Extensions
     {
-
+        public static Coordinates ToCoordinates(this Vector3 vector3)
+        {
+            return new Coordinates(vector3.x, vector3.y, vector3.z);
+        }
     }
 }
