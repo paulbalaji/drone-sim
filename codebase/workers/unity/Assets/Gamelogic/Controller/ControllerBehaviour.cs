0b5cc5d0efda530d94ec9d45c8517f5a970d5232
Paul Balaji <paulmonish@gmail.com>
2018-03-30 19:28:29 +0100

    added Controller schema + behaviour

---
+++ codebase/workers/unity/Assets/Gamelogic/Controller/ControllerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Improbable;
+using Improbable.Drone;
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+namespace Assets.Gamelogic.Controller
+{
+    [WorkerType(WorkerPlatform.UnityWorker)]
+    public class Controller : MonoBehaviour
+    {
+
+        private void OnEnable()
+        {
+            //register stuff
+        }
+
+        private void OnDisable()
+        {
+            //deregister stuff
+        }
+
+        void FixedUpdate()
+        {
+            //do stuff
+        }
+    }
+}
