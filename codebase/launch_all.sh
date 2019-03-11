cdaa484cab34ee50639e39aaa456e17941b51215
Paul Balaji <paulmonish@gmail.com>
2019-03-11 16:52:21 +0000

    update
    Signed-off-by: Paul Balaji <paulmonish@gmail.com>

--- codebase/launch_all.sh
+++ codebase/launch_all.sh
@@ -1,3 +1,3 @@
-spatial cloud launch dronesim_flagz_2 london_small_cloud_llv.json dronesim_2000_llv --snapshot=snapshots/london_small.snapshot
-spatial cloud launch dronesim_flagz_2 london_small_cloud_fcfs.json dronesim_2000_fcfs --snapshot=snapshots/london_small.snapshot
-spatial cloud launch dronesim_flagz_2 london_small_cloud_sjf.json dronesim_2000_sjf --snapshot=snapshots/london_small.snapshot
+spatial cloud launch dronesim_10mar london_small_cloud_llv.json dronesim_13mar_llv --snapshot=snapshots/london_small.snapshot
+spatial cloud launch dronesim_10mar london_small_cloud_fcfs.json dronesim_13mar_fcfs --snapshot=snapshots/london_small.snapshot
+spatial cloud launch dronesim_10mar london_small_cloud_sjf.json dronesim_13mar_sjf --snapshot=snapshots/london_small.snapshot
